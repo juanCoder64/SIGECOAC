4da84ab31f31c3d7f4946aed0906ed47a3935bf4
jgama <jgama12569@gmail.com>
2024-11-27 19:31:56 -0600

    esqueleto clases

--- main.py
+++ main.py
@@ -1,5 +1,51 @@
 import tkinter as tk
 
+class usuario:
+    def __init__(self, nombre, correo, contrasena):
+        self.nombre = nombre
+        self.correo = correo
+        self.contrasena= contrasena
+    def cancelarConsulta(self):
+        pass
+    def mostrarConsultas(self):
+        pass
+    def confirmarConsulta(self):
+        pass
+    def visualizarHorario(self):
+        pass
+    def iniciarSesion(self):
+        pass
+
+class estudiante(usuario):
+    def programarConsulta(self):
+        pass
+class profesor (usuario):
+    def actualizarDisponibilidad(self):
+        pass
+
+class calendario:
+    def __init__(self, horasDisponibles):
+        self.horasDisponibles=horasDisponibles;
+    def mostrarHorario(self):
+        pass
+    def modificaHorarioDisponible(self):
+        pass
+
+class Notificacion:
+    def __init__(self, estado, destinatario): #generar notificacion
+        self.estado=estado
+        self.destinatario=destinatario
+    def enviarNotificacion(self):
+        pass
+
+class consulta:
+    def __init__(self, estudiante, profesor, fecha, notas, estado):#Generar consulta
+        self.estudiante=estudiante
+        self.profesor=profesor
+        self.fecha=fecha
+        self.notas=notas
+        self.estado=estado
+
 estudiantes = [("estudiante", "contraseña")]
 profesores = [("profesor", "contraseña")]
 root = tk.Tk()
